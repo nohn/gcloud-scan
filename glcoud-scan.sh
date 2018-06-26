b669d4ddb03dd23c1a4c4fec3c662d44f3f01131
Sebastian Nohn <sebastian@nohn.net>
2018-06-26 12:06:44 +0200

    add Cloud SQL

--- glcoud-scan.sh
+++ glcoud-scan.sh
@@ -2,7 +2,7 @@
 
 SCANFOR="$1"
 
-declare -A SCOPES=(
+declare -A SIMPLESCOPES=(
     [sshkeys]="compute project-info describe"
     [instances]="compute instances list"
     [service-accounts]="iam service-accounts list"
@@ -13,10 +13,15 @@ for PROJECT in $(gcloud projects list --format="value(projectId)" | sort); do
     echo "Scanning Project $PROJECT..."
     echo " for IAM Users..."
     gcloud projects get-iam-policy "$PROJECT" | grep "$SCANFOR" | sort
-    # Iterate over all scopes
-    for SCOPE in "${!SCOPES[@]}"; do
-	echo " for $SCOPE..."
-	gcloud ${SCOPES[$SCOPE]} --project="$PROJECT" | grep "$SCANFOR" | sort
+    # Iterate over all simple scopes
+    for SIMPLESCOPE in "${!SIMPLESCOPES[@]}"; do
+	echo " for $SIMPLESCOPE..."
+	gcloud ${SIMPLESCOPES[$SIMPLESCOPE]} --project="$PROJECT" | grep "$SCANFOR" | sort
+    done
+    echo " for Cloud SQL Users..."
+    for SQLINSTANCE in $(gcloud sql instances list --project="$PROJECT" --filter STATUS:RUNNABLE --format="value(name)"); do
+	echo "  ... instance $SQLINSTANCE:"
+	gcloud sql users list --project="$PROJECT" --instance="$SQLINSTANCE" | grep "$SCANFOR" | sort
     done
     echo; echo
 done
