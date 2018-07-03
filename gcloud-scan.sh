4d6d9efb897c06868f16166a9cbedd3385912e19
Sebastian Nohn <sebastian@nohn.net>
2018-07-03 12:28:25 +0200

    always grep case-insensitive, always colorize output

--- gcloud-scan.sh
+++ gcloud-scan.sh
@@ -15,16 +15,16 @@ declare -A SIMPLESCOPES=(
 for PROJECT in $(gcloud projects list --format="value(projectId)" | sort); do
     echo "Scanning Project $PROJECT..."
     echo " for IAM Users..."
-    gcloud projects get-iam-policy "$PROJECT" | egrep "$SCANFOR" | egrep -v "$EXCLUDE" | sort
+    gcloud projects get-iam-policy "$PROJECT" | egrep -v "$EXCLUDE" | egrep -i --color=always "$SCANFOR" | sort
     # Iterate over all simple scopes
     for SIMPLESCOPE in "${!SIMPLESCOPES[@]}"; do
 	echo " for $SIMPLESCOPE..."
-	gcloud ${SIMPLESCOPES[$SIMPLESCOPE]} --project="$PROJECT" | egrep "$SCANFOR" | egrep -v "$EXCLUDE" | sort
+	gcloud ${SIMPLESCOPES[$SIMPLESCOPE]} --project="$PROJECT" | egrep -v "$EXCLUDE" | egrep -i --color=always "$SCANFOR" | sort
     done
     echo " for Cloud SQL Users..."
     for SQLINSTANCE in $(gcloud sql instances list --project="$PROJECT" --filter STATUS:RUNNABLE --format="value(name)"); do
 	echo "  ... instance $SQLINSTANCE:"
-	gcloud sql users list --project="$PROJECT" --instance="$SQLINSTANCE" | egrep "$SCANFOR" | egrep -v "$EXCLUDE" | sort
+	gcloud sql users list --project="$PROJECT" --instance="$SQLINSTANCE" | egrep -v "$EXCLUDE" | egrep -i --color=always "$SCANFOR" | sort
     done
     echo; echo
 done
